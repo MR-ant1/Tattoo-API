77adbdb77067e17affc5d4537d97c78fe254c40f
antonio <antoniorc7@gmail.com>
2024-02-26 20:51:26 +0100

    feats: funcionality of server and DB link done and first table in database API_TATTOO added

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import { app } from './app';
+import { AppDataSource } from './database/db';
+
+const PORT = process.env.PORT || 4002;
+
+
+const startServer = () => {
+    AppDataSource.initialize()
+        .then(() => {
+            console.log('database connected')
+
+            app.listen(PORT, () => {
+                console.log(`Server is running on port: ${PORT}`)
+            })
+        })
+
+        .catch(error => {
+            console.log(error);
+        })
+}
+startServer();
